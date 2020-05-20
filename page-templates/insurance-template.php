0a4cbe73a9bdfefbe9faf570e9c034868caf873e
Alex Boeger <alexboeger@gmail.com>
2020-05-19 20:04:36 -0500

    Updates for 1.1.0, see clickup for more details

--- page-templates/insurance-template.php
+++ page-templates/insurance-template.php
@@ -16,7 +16,9 @@ Template Post Type: insurance
 		<div class="container">
 			<h1><?php the_field('banner_title'); ?></h1>
 			<p><?php the_field('banner_text'); ?></p>
-			<button type="button" class="btn btn-outline btn-lg">Get A Quote</button>
+			<a href="/contact-us">
+				<button type="button" class="btn btn-outline btn-lg">Get A Quote</button>
+			</a>
 		</div>
 	</div>
 </div>
@@ -89,19 +91,13 @@ Template Post Type: insurance
 <div id="insurance-footer" class="container-fluid" style="background-image:url('<?php echo $image['url']; ?>')">
 	<div class="container-fluid">
 		<div class="row w-100">
-				<div class="col-md-2">
-
-				</div>
-				<div class="col-md-4">
+				<div class="col-md-6">
 					<h2>Ready To Get Started?</h2>
 					<p>Visit our <a href="/contact-us/">Contact Us Page</a> or <a href="tel:8558744677"> Talk To A Specialist</a></p>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-6">
 					<?php $link = get_field('get_a_quote_url'); ?>
 					<a href="<?php echo esc_url( $link ); ?>" ><button type="button" class="btn btn-md">Get A Quote</button></a>
-				</div>
-				<div class="col-md-2">
-
 				</div>
 		</div>
 		</div>
