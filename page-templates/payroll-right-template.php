35111d3e1b0b8b1fec4094ead7f6c04939427f77
Alex Boeger <alexboeger@gmail.com>
2020-06-01 20:19:22 -0500

    Payroll Right Template Added

---
+++ page-templates/payroll-right-template.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+Template Name: Payroll Elementor Right
+Template Post Type: payroll
+*/
+?>
+
+<?php get_header();?>
+
+
+<!--- Payroll Header --->
+<?php $image = get_field('banner_image'); ?>
+<div id="payroll-header" class="container-fluid" style="background-image:url('<?php echo $image['url']; ?>')">
+	<div class="container-fluid">
+		<div class="container">
+			<h1><?php the_field('banner_title'); ?></h1>
+			<p><?php the_field('banner_text'); ?></p>
+			<button type="button" class="btn btn-outline-success btn-lg">Schedule Live Demo</button>
+		</div>
+	</div>
+</div>
+
+
+
+
+<!--- Main Content --->
+		<div id="payroll-content-container"class="container-fluid">
+			<section class="page-wrap">
+				<div class="row">
+				<div class="col-md-3">
+					    <!-- Payroll Menu -->
+							<div class="">
+						<?php wp_nav_menu( array(
+							'theme_location' 	=> 'payroll-menu',
+							'fallback_cb'    	=> '__return_false',
+							'depth'           	=> 0,
+							'container'     	=> 'div',
+							'container_class' 	=> 'side-menu',
+							'walker'			=> new Walker_Nav_Side(),
+						) ); ?>
+							</div>
+				</div>
+				<div class="col content-right">
+					<?php while (have_posts()) : the_post();/* Start loop */ ?>
+						<?php the_content(); ?>
+					<?php endwhile; /* End loop */ ?>
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+
+<!--- Payroll Footer --->
+<?php $image = get_field('footer_image'); ?>
+<div id="payroll-footer" class="container-fluid" style="background-image:url('<?php echo $image['url']; ?>')">
+	<div class="container-fluid">
+		<div class="container">
+			<h1><?php the_field('footer_title'); ?></h1>
+			<p><?php the_field('footer_text'); ?></p>
+			<button type="button" class="btn btn-success btn-lg">Schedule Live Demo</button>
+		</div>
+	</div>
+</div>
+
+<?php get_footer();?>
