14df0bec8cddd39fbbebd367334fae96e11432f3
Alex Boeger <alexboeger@gmail.com>
2020-07-06 16:50:00 -0500

    Added category buttons to archive page / selected category filter

--- archive.php
+++ archive.php
@@ -51,6 +51,40 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 
 			<div class="col w-100">
+                <div class="row" id="blog-category-buttons" >
+                    <div class="col-sm-4">
+                        <div class="d-flex flex-column" >
+                            <a href="/category/personal-lines-insurance/" class="btn btn-insurance">
+                                Personal Insurance
+                            </a>
+                            <a href="/category/commercial-insurance/" class="btn btn-insurance">
+                                Commerical Insurance
+                            </a>
+                            <a href="/category/payroll/" class="btn btn-insurance">
+                                Payroll
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="d-flex flex-column">
+                            <a href="/category/personal-lines-insurance/" class="btn btn-risk">
+                                COVID-19
+                            </a>                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="d-flex flex-column" >
+                            <a href="/category/risk-management/" class="btn btn-insurance">
+                                Risk Management
+                            </a>
+                            <a href="/category/human-resources/" class="btn btn-insurance">
+                                Human Resources
+                            </a>
+                            <a href="/category/health-benefits/" class="btn btn-insurance">
+                                Health & Benefits
+                            </a>
+                        </div>
+                    </div>
+                </div>
 				<div class="d-flex flex-wrap">
 
 						<?php if ( have_posts() ) : ?>
