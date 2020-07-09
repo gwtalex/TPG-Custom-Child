030b5001fa2871313fa08860c571804aeb9c13b2
Alex Boeger <alexboeger@gmail.com>
2020-07-09 12:59:02 -0500

    Added payroll form and styling to payroll header

--- partials/payroll-banner.php
+++ partials/payroll-banner.php
@@ -4,15 +4,9 @@
 		<div class="container">
 			<h1><?php the_field('banner_title'); ?></h1>
 			<p><?php the_field('banner_text'); ?></p>
-			<a href="<?php if ( get_field( 'cta_page' ) ): ?>
-
-				<?php the_field('cta_page'); ?>
-
-			<?php else: // field_name returned false ?>
-
-				<?php echo "/payroll/schedule-live-demo/"; ?>
-
-			<?php endif; // end of if field_name logic ?>"><button type="button" class="btn btn-outline-success btn-lg">Schedule Live Demo</button></a>
+			<div class="col-md-4 payroll-banner-form">
+				<?php echo do_shortcode('[super_form id="4963"]'); ?>
+			</div>
 		</div>
 	</div>
 </div>
